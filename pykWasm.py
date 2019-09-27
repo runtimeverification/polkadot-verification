82c8af1655bf87be4d46d481d0053988e4c13d04
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-09-27 16:53:33 -0600

    pykWasm: separate definitions and symbols for each compiled form

--- pykWasm.py
+++ pykWasm.py
@@ -11,32 +11,46 @@ from pyk.kast import KApply, KConstant, KSequence, KToken, KVariable, _notif, _w
 # Load Definition Specific Stuff                                               #
 ################################################################################
 
-WASM_definition = pyk.readKastTerm('.build/defn/kwasm/llvm/wasm-with-k-term-kompiled/compiled.json')
+WASM_definition_llvm_no_coverage_dir = '.build/defn/kwasm/llvm'
+WASM_definition_llvm_coverage_dir    = '.build/defn/coverage/llvm'
 
-WASM_symbols = pyk.buildSymbolTable(WASM_definition)
+WASM_definition_haskell_no_coverage_dir = '.build/defn/kwasm/haskell'
+WASM_definition_haskell_coverage_dir    = '.build/defn/coverage/haskell'
+
+WASM_definition_llvm_no_coverage = pyk.readKastTerm(WASM_definition_llvm_no_coverage_dir + '/wasm-with-k-term-kompiled/compiled.json')
+WASM_definition_llvm_coverage    = pyk.readKastTerm(WASM_definition_llvm_coverage_dir + '/wasm-with-k-term-kompiled/compiled.json')
+
+WASM_definition_haskell_no_coverage = pyk.readKastTerm(WASM_definition_haskell_no_coverage_dir + '/wasm-with-k-term-kompiled/compiled.json')
+WASM_definition_haskell_coverage    = pyk.readKastTerm(WASM_definition_haskell_coverage_dir + '/wasm-with-k-term-kompiled/compiled.json')
+
+WASM_symbols_llvm_no_coverage = pyk.buildSymbolTable(WASM_definition_llvm_no_coverage)
+WASM_symbols_llvm_coverage    = pyk.buildSymbolTable(WASM_definition_llvm_coverage)
+
+WASM_symbols_haskell_no_coverage = pyk.buildSymbolTable(WASM_definition_haskell_no_coverage)
+WASM_symbols_haskell_coverage    = pyk.buildSymbolTable(WASM_definition_haskell_coverage)
 
 # Custom unparsers for some symbols
-# WASM_symbols [ '.ValStack_WASM-DATA_'                                               ]  = pyk.constLabel('.ValStack')
-# WASM_symbols [ '.Int_WASM-DATA_'                                                    ]  = pyk.constLabel('.Int')
-# WASM_symbols [ '.ModuleInstCellMap'                                                 ]  = pyk.constLabel('.ModuleInstCellMap')
-# WASM_symbols [ '.FuncDefCellMap'                                                    ]  = pyk.constLabel('.FuncDefCellMap')
-# WASM_symbols [ '.TabInstCellMap'                                                    ]  = pyk.constLabel('.TabInstCellMap')
-# WASM_symbols [ '.MemInstCellMap'                                                    ]  = pyk.constLabel('.MemInstCellMap')
-# WASM_symbols [ '.GlobalInstCellMap'                                                 ]  = pyk.constLabel('.GlobalInstCellMap')
-# WASM_symbols [ 'ModuleInstCellMapItem'                                              ]  = (lambda a1, a2: a2)
-# WASM_symbols [ '___WASM__Stmt_Stmts'                                                ]  = (lambda a1, a2: a1 + '\n' + a2)
-# WASM_symbols [ '___WASM__Defn_Defns'                                                ]  = (lambda a1, a2: a1 + '\n' + a2)
-# WASM_symbols [ '___WASM__Instr_Instrs'                                              ]  = (lambda a1, a2: a1 + '\n' + a2)
-# WASM_symbols [ '.List{"___WASM__Stmt_Stmts_Stmts"}_Stmts'                           ]  = pyk.constLabel('')
-# WASM_symbols [ '.List{"___WASM__EmptyStmt_EmptyStmts_EmptyStmts"}_EmptyStmts'       ]  = pyk.constLabel('')
-# WASM_symbols [ '.List{"___WASM-DATA__ValType_ValTypes_ValTypes"}_ValTypes'          ]  = pyk.constLabel('')
-# WASM_symbols [ '.List{"___WASM__TypeDecl_TypeDecls_TypeDecls"}_TypeDecls'           ]  = pyk.constLabel('')
-# WASM_symbols [ '.List{"___WASM__LocalDecl_LocalDecls_LocalDecls"}_LocalDecls'       ]  = pyk.constLabel('')
-# WASM_symbols [ '.List{"___WASM-DATA__Index_ElemSegment_ElemSegment"}_ElemSegment'   ]  = pyk.constLabel('')
-# WASM_symbols [ '.List{"___WASM-DATA__WasmString_DataString_DataString"}_DataString' ]  = pyk.constLabel('')
-# WASM_symbols [ '(module__)_WASM__OptionalId_Defns'                                  ]  = (lambda mName, mDefns: '(module ' + mName + '\n' + pyk.indent(mDefns) + '\n)')
-# WASM_symbols [ '(func__)_WASM__OptionalId_FuncSpec'                                 ]  = (lambda funcName, funcSpec: '(func ' + funcName + '\n' + pyk.indent(funcSpec) + '\n)')
-# WASM_symbols [ '____WASM__TypeUse_LocalDecls_Instrs'                                ]  = (lambda type, locals, instrs: '\n'.join([type, locals, instrs]))
+# WASM_symbols [ '.ValStack_WASM-DATA_'                                               ] = pyk.constLabel('.ValStack')
+# WASM_symbols [ '.Int_WASM-DATA_'                                                    ] = pyk.constLabel('.Int')
+# WASM_symbols [ '.ModuleInstCellMap'                                                 ] = pyk.constLabel('.ModuleInstCellMap')
+# WASM_symbols [ '.FuncDefCellMap'                                                    ] = pyk.constLabel('.FuncDefCellMap')
+# WASM_symbols [ '.TabInstCellMap'                                                    ] = pyk.constLabel('.TabInstCellMap')
+# WASM_symbols [ '.MemInstCellMap'                                                    ] = pyk.constLabel('.MemInstCellMap')
+# WASM_symbols [ '.GlobalInstCellMap'                                                 ] = pyk.constLabel('.GlobalInstCellMap')
+# WASM_symbols [ 'ModuleInstCellMapItem'                                              ] = (lambda a1, a2: a2)
+# WASM_symbols [ '___WASM__Stmt_Stmts'                                                ] = (lambda a1, a2: a1 + '\n' + a2)
+# WASM_symbols [ '___WASM__Defn_Defns'                                                ] = (lambda a1, a2: a1 + '\n' + a2)
+# WASM_symbols [ '___WASM__Instr_Instrs'                                              ] = (lambda a1, a2: a1 + '\n' + a2)
+# WASM_symbols [ '.List{"___WASM__Stmt_Stmts_Stmts"}_Stmts'                           ] = pyk.constLabel('')
+# WASM_symbols [ '.List{"___WASM__EmptyStmt_EmptyStmts_EmptyStmts"}_EmptyStmts'       ] = pyk.constLabel('')
+# WASM_symbols [ '.List{"___WASM-DATA__ValType_ValTypes_ValTypes"}_ValTypes'          ] = pyk.constLabel('')
+# WASM_symbols [ '.List{"___WASM__TypeDecl_TypeDecls_TypeDecls"}_TypeDecls'           ] = pyk.constLabel('')
+# WASM_symbols [ '.List{"___WASM__LocalDecl_LocalDecls_LocalDecls"}_LocalDecls'       ] = pyk.constLabel('')
+# WASM_symbols [ '.List{"___WASM-DATA__Index_ElemSegment_ElemSegment"}_ElemSegment'   ] = pyk.constLabel('')
+# WASM_symbols [ '.List{"___WASM-DATA__WasmString_DataString_DataString"}_DataString' ] = pyk.constLabel('')
+# WASM_symbols [ '(module__)_WASM__OptionalId_Defns'                                  ] = (lambda mName, mDefns: '(module ' + mName + '\n' + pyk.indent(mDefns) + '\n)')
+# WASM_symbols [ '(func__)_WASM__OptionalId_FuncSpec'                                 ] = (lambda funcName, funcSpec: '(func ' + funcName + '\n' + pyk.indent(funcSpec) + '\n)')
+# WASM_symbols [ '____WASM__TypeUse_LocalDecls_Instrs'                                ] = (lambda type, locals, instrs: '\n'.join([type, locals, instrs]))
 
 ################################################################################
 # Runner Wrappers                                                              #
@@ -72,7 +86,7 @@ if __name__ == '__main__':
         sys.exit(returnCode)
 
     # Use fast pyk unparser to get string for initial configuration
-    fastPrinted = pyk.prettyPrintKast(initial_configuration['args'][0], WASM_symbols)
+    fastPrinted = pyk.prettyPrintKast(initial_configuration['args'][0], WASM_symbols_llvm_no_coverage)
     _notif('fastPrinted output')
     print(fastPrinted)
 
