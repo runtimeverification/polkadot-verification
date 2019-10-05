2016516cb6e056da24428563aef76ae342002363
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-10-05 17:00:40 -0600

    mergeRules: make operate as library too

--- mergeRules.py
+++ mergeRules.py
@@ -7,12 +7,6 @@ import tempfile
 from pykWasm import *
 from pykWasm import _notif, _warning, _fatal
 
-rules = []
-with open(sys.argv[1], 'r') as rule_file:
-    rules = [ line.strip() for line in rule_file ]
-
-subsequence_length = int(sys.argv[2])
-
 ################################################################################
 # Should be Upstreamed                                                         #
 ################################################################################
@@ -34,7 +28,7 @@ def _runK2(command, definition, kArgs = [], teeOutput = True, kRelease = None):
     _notif('Running: ' + ' '.join(kCommand))
     return pyk._teeProcessStdout(kCommand, tee = teeOutput)
 
-def mergeRules(definition, ruleList, kArgs = [], teeOutput = True, kRelease = None):
+def mergeRulesKoreExec(definition, ruleList, kArgs = [], teeOutput = True, kRelease = None):
     with tempfile.NamedTemporaryFile(mode = 'w') as tempf:
         tempf.write('\n'.join(ruleList))
         tempf.flush()
@@ -46,56 +40,99 @@ def mergeRules(definition, ruleList, kArgs = [], teeOutput = True, kRelease = No
 # Calculate subsequences and find the maximal subsequences                     #
 ################################################################################
 
-subsequences = { }
+def maximal_nonoverlapping_subsequences(sequence, subsequence_length = 2):
+    """Calculate maximal length non-overlapping subsequences from a given sequence of strings.
 
-for i in range(0, len(rules) - subsequence_length):
-    subsequence = '|'.join(rules[ i : i + subsequence_length ])
-    if subsequence in subsequences:
-        subsequences[subsequence] += 1
-    else:
-        subsequences[subsequence] = 1
+    Input:
 
-maximal_subsequences          = []
-maximal_subsequence_count     = 0
-maximal_subsequences_rule_set = set([])
+        -   sequence: list of strings.
+        -   subsequence_length: size of subsequences to look for.
 
-for subsequence in subsequences.keys():
-    rule_set = set(subsequence.split('|'))
-    if subsequences[subsequence] > maximal_subsequence_count:
-        maximal_subsequences          = [subsequence]
-        maximal_subsequence_count     = subsequences[subsequence]
-        maximal_subsequences_rule_set = rule_set
-    elif subsequences[subsequence] == maximal_subsequence_count:
-        if not any([ rule in maximal_subsequences_rule_set for rule in rule_set ]):
-            maximal_subsequences.append(subsequence)
-            maximal_subsequences_rule_set = maximal_subsequences_rule_set.union(rule_set)
+    Output: maximal non-overlapping subsequences, with earlier subsequences preferred.
+    """
+    subsequences = { }
+
+    for i in range(0, len(sequence) - subsequence_length):
+        subsequence = '|'.join(sequence[ i : i + subsequence_length ])
+        if subsequence in subsequences:
+            subsequences[subsequence] += 1
+        else:
+            subsequences[subsequence] = 1
+
+    maximal_subsequences          = []
+    maximal_subsequence_count     = 0
+    maximal_subsequences_rule_set = set([])
+
+    for subsequence in subsequences.keys():
+        rule_set = set(subsequence.split('|'))
+        if subsequences[subsequence] > maximal_subsequence_count:
+            maximal_subsequences          = [subsequence]
+            maximal_subsequence_count     = subsequences[subsequence]
+            maximal_subsequences_rule_set = rule_set
+        elif subsequences[subsequence] == maximal_subsequence_count:
+            if not any([ rule in maximal_subsequences_rule_set for rule in rule_set ]):
+                maximal_subsequences.append(subsequence)
+                maximal_subsequences_rule_set = maximal_subsequences_rule_set.union(rule_set)
+
+    return [ subsequence.split('|') for subsequence in maximal_subsequences ]
 
 ################################################################################
 # Generate merged rules for selected maximal subsequences                      #
 ################################################################################
 
-for subsequence in maximal_subsequences:
-    _notif('Merging rules: ' + subsequence)
-    rule_ids = subsequence.split('|')
-    for rule_id in rule_ids:
-        print('rule_id: ' + rule_id)
-        rule = getRuleById(WASM_definition_haskell_no_coverage, rule_id)
-        print(pyk.prettyPrintKast(rule, WASM_symbols_haskell_no_coverage))
-        print()
+def mergeRules(rule_sequence, definition_dir, definition, main_defn_file, main_module, symbol_table, subsequence_length = 2):
+    """Merge rules which form maximal length subsequences.
 
-    (rc, stdout, stderr) = mergeRules(WASM_definition_haskell_no_coverage_dir + '/wasm-with-k-term-kompiled/definition.kore', rule_ids, kArgs = ['--module', 'WASM-WITH-K-TERM'])
-    if rc == 0:
-        with tempfile.NamedTemporaryFile(mode = 'w') as tempf:
-            tempf.write(stdout)
-            tempf.flush()
-            (_, stdout, stderr) = pyk.kast(WASM_definition_haskell_no_coverage_dir, tempf.name, kastArgs = ['--input', 'kore', '--output', 'json'])
-            merged_rule = json.loads(stdout)['term']
-            rule_pattern = KRewrite(KApply('#And', [KVariable('#CONSTRAINT'), KVariable('#INITTERM')]), KVariable('#FINALTERM'))
-            rule_subst = pyk.match(rule_pattern, merged_rule)
-            rule_body = pyk.KRewrite(rule_subst['#INITTERM'], rule_subst['#FINALTERM'])
-            gen_rule = pyk.KRule(rule_body, requires = rule_subst['#CONSTRAINT'])
-            print(pyk.prettyPrintKast(gen_rule, WASM_symbols_haskell_no_coverage))
-            sys.stdout.flush()
-    else:
-        print(stderr)
-        _warning('Cannot merge rules!')
+    Input:
+        -   rule_sequence: total sequence of all rules to look at.
+        -   definition: JSON encoding of definition to do rule merging over.
+        -   main_defn_file: name of main definition file.
+        -   main_module: name of main module of definition.
+        -   symbol_table: unparsing symbol table for definition.
+        -   subsequence_length: how large of subsequences to attempt rule merging for.
+
+    Output: List of merged rules for maximal subsequences of the given length.
+    """
+    maximal_subsequences = maximal_nonoverlapping_subsequences(rule_sequence, subsequence_length = subsequence_length)
+
+    merged_rules = []
+    for subsequence in maximal_subsequences:
+        _notif('Merging rules: ' + str(subsequence))
+        for rule_id in subsequence:
+            print('rule_id: ' + rule_id)
+            rule = getRuleById(definition, rule_id)
+            print(pyk.prettyPrintKast(rule, symbol_table))
+            print()
+
+        (rc, stdout, stderr) = mergeRulesKoreExec(definition_dir + '/' + main_defn_file + '-kompiled/definition.kore', subsequence, kArgs = ['--module', main_module])
+        if rc == 0:
+            with tempfile.NamedTemporaryFile(mode = 'w') as tempf:
+                tempf.write(stdout)
+                tempf.flush()
+                (_, stdout, stderr) = pyk.kast(definition_dir, tempf.name, kastArgs = ['--input', 'kore', '--output', 'json'])
+                merged_rule = json.loads(stdout)['term']
+                rule_pattern = KRewrite(KApply('#And', [KVariable('#CONSTRAINT'), KVariable('#INITTERM')]), KVariable('#FINALTERM'))
+                rule_subst = pyk.match(rule_pattern, merged_rule)
+                rule_body = pyk.KRewrite(rule_subst['#INITTERM'], rule_subst['#FINALTERM'])
+                gen_rule = pyk.KRule(rule_body, requires = rule_subst['#CONSTRAINT'])
+                print(pyk.prettyPrintKast(gen_rule, WASM_symbols_haskell_no_coverage))
+                sys.stdout.flush()
+        else:
+            print(stderr)
+            _warning('Cannot merge rules!')
+
+    return merged_rules
+
+################################################################################
+# Main functionality                                                           #
+################################################################################
+
+if __name__ == '__main__':
+    rules_file_path    = sys.argv[1]
+    subsequence_length = int(sys.argv[2])
+
+    rules = []
+    with open(rules_file_path, 'r') as rules_file:
+        rules = [ line.strip() for line in rules_file ]
+
+    mergeRules(rules, WASM_definition_haskell_no_coverage_dir, WASM_definition_haskell_no_coverage, 'wasm-with-k-term', 'WASM-WITH-K-TERM', WASM_symbols_haskell_no_coverage, subsequence_length = subsequence_length)
