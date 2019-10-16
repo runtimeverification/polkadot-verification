b84b289ae08be835adab4f769381df652a79d098
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-10-15 23:29:27 -0600

    mergeRulesIterate: actually write resulting new module to file

--- mergeRulesIterate.py
+++ mergeRulesIterate.py
@@ -29,23 +29,26 @@ def get_coverage(input_program_path, src_definition_dir, main_defn_file, dst_def
 def append_module_to_file(definition_dir, main_defn_file, main_module, iteration_number, new_rules, symbol_table):
     module_import = main_module if iteration_number == 0 else main_module + '-' + str(iteration_number - 1)
     new_module = KModule(main_module + '-' + str(iteration_number), [module_import], new_rules)
-    new_module_str = pyk.prettyPrintKast(new_module, symbol_table)
-    APPEND_TO_FILE(definition_dir + '/' + main_defn_file + '.k', new_module_str)
+    new_module_str = '\n\n' + pyk.prettyPrintKast(new_module, symbol_table)
+    with open(definition_dir + '/' + main_defn_file + '.k', 'a') as defn_file:
+        defn_file.write(new_module_str)
 
 def iterated_compile(src_definition_dir, dst_definition_dir, main_defn_file, main_module, input_program, subsequence_length, iterations):
-    src_definition_json = pyk.kastReadTerm(src_definition_dir + '/' + main_defn_file + '-kompiled/compiled.json')
-    dst_definition_json = pyk.kastReadTerm(dst_definition_dir + '/' + main_defn_file + '-kompiled/compiled.json')
+    src_definition_json = pyk.readKastTerm(src_definition_dir + '/' + main_defn_file + '-kompiled/compiled.json')
+    dst_definition_json = pyk.readKastTerm(dst_definition_dir + '/' + main_defn_file + '-kompiled/compiled.json')
 
     src_symbol_table = pyk.buildSymbolTable(src_definition_json)
     dst_symbol_table = pyk.buildSymbolTable(dst_definition_json)
 
     times = []
     for i in range(iterations):
-        kompile_definition(src_definition_dir, main_defn_file, main_module)
-        kompile_definition(dst_definition_dir, main_defn_file, main_module)
-        time_start = time.clock()
+        print(src_definition_dir)
+        sys.stdout.flush()
+        kompile_definition(src_definition_dir, 'llvm',    src_definition_dir + '/' + main_defn_file + '.k', main_module)
+        kompile_definition(dst_definition_dir, 'haskell', dst_definition_dir + '/' + main_defn_file + '.k', main_module)
+        time_start = time.perf_counter()
         coverage_data = get_coverage(input_program, src_definition_dir, main_defn_file, dst_definition_dir)
-        times.append(time.clock() - time_start)
+        times.append(time.perf_counter() - time_start)
         merged_rules = mergeRules(coverage_data, dst_definition_json, main_defn_file, main_module, dst_symbol_table, subsequence_length = subsequence_length)
         merged_rules = [ addAttribute(rule, 'priority', str(49 - i)) for rule in merged_rules ]
         append_module_to_file(src_definition_dir, main_defn_file, main_module, i, merged_rules, src_symbol_table)
@@ -53,13 +56,13 @@ def iterated_compile(src_definition_dir, dst_definition_dir, main_defn_file, mai
     return times
 
 if __name__ == '__main__':
-    src_definition_dir = sys.argv[0]      # .build/defn/coverage/llvm
-    dst_definition_dir = sys.argv[1]      # .build/defn/kwasm/haskell
-    main_defn_file     = sys.argv[2]      # wasm-with-k-term
-    main_module        = sys.argv[3]      # WASM-WITH-K-TERM
-    input_program      = sys.argv[4]      # deps/wasm-semantics/tests/simple/constants.wast
-    subsequence_length = int(sys.argv[5]) # 2
-    iterations         = int(sys.argv[6]) # 5
+    src_definition_dir = sys.argv[1]      # .build/defn/coverage/llvm
+    dst_definition_dir = sys.argv[2]      # .build/defn/kwasm/haskell
+    main_defn_file     = sys.argv[3]      # wasm-with-k-term
+    main_module        = sys.argv[4]      # WASM-WITH-K-TERM
+    input_program      = sys.argv[5]      # deps/wasm-semantics/tests/simple/constants.wast
+    subsequence_length = int(sys.argv[6]) # 2
+    iterations         = int(sys.argv[7]) # 5
 
     times = iterated_compile(src_definition_dir, dst_definition_dir, main_defn_file, main_module, input_program, subsequence_length, iterations)
     print(times)
