3113efa1b192b635f775a7f569c60c5418d57fab
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-10-24 23:25:59 -0600

    strip-erroneous-labels: comment about use of file

--- strip-erroneous-labels.sh
+++ strip-erroneous-labels.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Currently we are importing K-TERM as a hack to get around various unparsing issues when using `pyk` library.
+# K-TERM defines three separate productions with the same KLabel/Symbol for `#EmptyK`, and two separate ones for `#EmptyKList`.
+# The Haskell backend rejects definitions which have multiple definitions of the same symbol, so this causes it to crash.
+# This file forcibly strips all but one of the occurances of each label from the resulting `definition.kore`.
+
 set -euo pipefail
 
 input_definition="$1" && shift
