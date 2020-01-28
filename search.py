c0c421192f525f55c6f4b31126719db25ebb3190
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-01-28 20:33:51 +0000

    search: more fixes for updated K

--- search.py
+++ search.py
@@ -10,10 +10,16 @@ sys.setrecursionlimit(150000000)
 
 kastProgram = pyk.readKastTerm('src/polkadot-runtime.wat.json')
 
-function_name = '$srml_balances::Module<T_I>::set_free_balance::h740a36cc4860a8fe'
+function_names = [ '$pallet_balances::Module<T_I>::set_free_balance::h143784e9433faed6'
+                 , '$pallet_balances::Module<T_I>::call_functions::h5c8befb10787dea0'
+                 , '$pallet_balances::Module<T_I>::storage_metadata::h082815e2817e5c19'
+                 , '$pallet_balances::Module<T_I>::module_constants_metadata::h487a5f31fed8642e'
+                 ]
+
+function_name = function_names[0]
 
 wasm_invoke = lambda fid: KApply('(invoke_)_WASM_Instr_Int', [KToken(str(fid), 'Int')])
-wasm_push = lambda type, value: KApply('(_)_WASM-TEXT_FoldedInstr_PlainInstr', [KApply('_.const__WASM_PlainInstr_IValType_Int', [KApply(type + '_WASM-DATA_IValType', []), value])])
+wasm_push = lambda type, value: KApply('(_)_WASM-TEXT_FoldedInstr_PlainInstr', [KApply('_.const__WASM_PlainInstr_IValType_Int', [KConstant(type + '_WASM-DATA_IValType'), value])])
 
 def wasm_stmts_join(stmtType = 'Stmt'):
     return '___WASM_' + stmtType + 's_' + stmtType + '_' + stmtType + 's'
@@ -49,7 +55,7 @@ invoking_steps = init_stmts                          \
                  , wasm_invoke(156)
                  ]
 
-init_subst['K_CELL'] = wasm_stmts(invoking_steps)
+init_subst['K_CELL'] = KSequence([wasm_stmts(invoking_steps)])
 
 init_config = pyk.substitute(symbolic_config, init_subst)
 
@@ -60,8 +66,10 @@ invokingSubstitution = { 'V1' : KToken(str(random.randint(0, 2 ** 32)), 'Int')
 
 init_config = pyk.substitute(init_config, invokingSubstitution)
 
-print(init_config)
 print(pyk.prettyPrintKast(init_config, WASM_symbols_llvm_no_coverage))
-(_, after_running, _) = krun({ 'format' : 'KAST' , 'version': 1, 'term': init_config })
+sys.stdout.flush()
+(_, after_running, _) = krun({ 'format' : 'KAST' , 'version': 1, 'term': init_config }, '--term')
+sys.stdout.flush()
+sys.stderr.flush()
 print(pyk.prettyPrintKast(after_running, WASM_symbols_llvm_no_coverage))
 sys.stdout.flush()
