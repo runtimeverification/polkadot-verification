69a2c111d37e539293b36f46239b7e360c609965
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-05-26 12:42:04 +0000

    build-runtime-host.sh: handle imported memories as well

--- build-runtime-host.sh
+++ build-runtime-host.sh
@@ -7,8 +7,9 @@ set -euo pipefail
 input_wat="$1" ; shift
 
 echo '(module'
-grep '^  (type'         "$input_wat"
-grep '^  (import "env"' "$input_wat" | sed 's/  (import "env" \("[a-z1-9_]*"\) (func \(\$[a-z1-9_]*\) \((type [0-9]*)\)))/  (func \2 (export \1) \3 (phost . \2))/'
+grep '^  (type'                  "$input_wat"
+grep '^  (import "env".*(memory' "$input_wat" | sed 's/  (import "env" \("[a-z1-9_]*"\) (memory (;[0-9]*;) \([0-9]*\)))/  (memory (export \1) \2)/'
+grep '^  (import "env".*(func'   "$input_wat" | sed 's/  (import "env" \("[a-z1-9_]*"\) (func \(\$[a-z1-9_]*\) \((type [0-9]*)\)))/  (func \2 (export \1) \3 (phost . \2))/'
 echo ')'
 echo ''
 echo '(register "env")'
